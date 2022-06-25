a94a9c282a8bafb8160266bdb21c824312483118
Yasser <siryasser2020@gmail.com>
2022-06-25 03:32:58 +0200

    Update setting-box And Add About Sec

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /*
                 Setting Box
 */
+// Setting
 // Select Setting  Element
 document.querySelector(".toggle-setting").addEventListener("click", () => {
   document.querySelector(".setting-box i").classList.toggle("fa-spin");
@@ -8,6 +9,9 @@ document.querySelector(".toggle-setting").addEventListener("click", () => {
   document.querySelector(".setting-box").classList.toggle("options");
 });
 
+/*
+                Color
+*/
 // Check Local Storage Color
 let allLi = document.querySelectorAll(".color-list li");
 
@@ -45,6 +49,59 @@ allLi.forEach(function (li) {
     localStorage.setItem("color-opt", e.target.dataset.color);
   });
 });
+/*
+                BackGround Random              
+*/
+
+// Switch BackGround Opt
+let random = document.querySelectorAll(".random-back span");
+
+// Random Background Option
+let backgroundOption = true;
+
+// Control To Interval
+let backgroundInterval;
+
+// Check If  There is Local Storage Random Background Item
+let backgroundLocalItem = window.localStorage.getItem("background-option");
+
+// Check In backgroundLocalItem Empty or Not
+if (backgroundLocalItem !== null) {
+  // console.log(typeof backgroundLocalItem);
+  console.log(backgroundLocalItem);
+  if (backgroundLocalItem === "true") {
+    backgroundOption = true;
+  } else {
+    backgroundOption = false;
+  }
+  random.forEach((span) => {
+    span.classList.remove("active");
+  });
+  if (backgroundLocalItem === "true") {
+    document.querySelector(".random-back .yes").classList.add("active");
+  } else {
+    document.querySelector(".random-back .no").classList.add("active");
+  }
+}
+
+// Loop On All Spans
+random.forEach((e) => {
+  e.addEventListener("click", (ac) => {
+    random.forEach((span) => {
+      span.classList.remove("active");
+    });
+    ac.target.classList.add("active");
+    if (ac.target.getAttribute("data-background") === "yes") {
+      backgroundOption = true;
+      randomizeImgs();
+      localStorage.setItem("background-option", true);
+    } else {
+      backgroundOption = false;
+      clearInterval(backgroundInterval);
+      localStorage.setItem("background-option", false);
+    }
+  });
+});
 
 /*
                 Landing
@@ -61,12 +118,24 @@ let imgsArray = [
   "landing-5.jpg",
 ];
 
-setInterval(() => {
-  // Get Random Number
-  let randomNumber = Math.floor(Math.random() * imgsArray.length);
-  // Change Background Image Url
-  landingPage.style.backgroundImage = `url("imgs/${imgsArray[randomNumber]}")`;
-}, 5000);
+// // Random Background Option
+// let backgroundOption = true;
+
+// // Control To Interval
+// let backgroundInterval;
+
+// Function To Randomize Imgs
+function randomizeImgs() {
+  if (backgroundOption === true) {
+    backgroundInterval = setInterval(() => {
+      // Get Random Number
+      let randomNumber = Math.floor(Math.random() * imgsArray.length);
+      // Change Background Image Url
+      landingPage.style.backgroundImage = `url("imgs/${imgsArray[randomNumber]}")`;
+    }, 5000);
+  }
+}
+randomizeImgs();
 
 // Select Bar  Element
 let bar = document.querySelector(".select-js");
